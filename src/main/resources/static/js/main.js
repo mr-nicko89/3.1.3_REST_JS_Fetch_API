bdf5cfae427b9ae3ab71cfa158b0a4c300a11635
Nickolay <mr-nicko89@yandex.ru>
2021-06-21 20:31:00 +0300

    2.6 Реализовать подгрузку из rest-контроллера данных +2.6.6 Данные в левой вкладке User

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -56,13 +56,27 @@ function getPrincipal() {
         }
         return response.json();
     }).then(dataPrincipal => {
-            const htmlPrincipal = `<span class="navbar-brand" href="#">${dataPrincipal.email} with roles: ${
-                dataPrincipal.roleSet.map(roles => roles.name).join("")
+            const htmlHeaderPrincipal = `<span class="navbar-brand" href="#">${dataPrincipal.email} with roles: ${
+                dataPrincipal.roleSet.map(roles => roles.name).join(" ")
             }</span>`;
-            console.log(htmlPrincipal);
-        document
-            .querySelector('#principal')
-            .insertAdjacentHTML("afterbegin", htmlPrincipal);
+            // console.log(htmlPrincipal);
+            document
+                .querySelector('#principal')
+                .insertAdjacentHTML("afterbegin", htmlHeaderPrincipal);
+
+            const htmlActiveUser = `
+                <tr class="border-top">
+                    <td>${dataPrincipal.name}</td>
+                    <td>${dataPrincipal.age}</td>
+                    <td>${dataPrincipal.email}</td>
+                    <td>${dataPrincipal.password}</td>
+                    <td>${dataPrincipal.roleSet.map(roles => roles.name)}</td>
+                </tr>
+`;
+            document
+                .querySelector('#activeUser')
+                .insertAdjacentHTML("afterbegin", htmlActiveUser);
+            // console.log(htmlActiveUser)
         }
     ).catch(error => {
         console.log(error);
